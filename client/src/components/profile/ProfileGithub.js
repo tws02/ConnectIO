be9ac961c9bbece0fd3b4d807361799553538d7e
Wei Sheng Tan <tanweisheng02@gmail.com>
2020-08-19 01:22:28 -0700

    github repos

---
+++ client/src/components/profile/ProfileGithub.js
@@ -0,0 +1,54 @@
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { getRepos } from "../../actions/profile";
+import Spinner from "../layout/Spinner";
+
+const ProfileGithub = ({ username, getRepos, repos }) => {
+  useEffect(() => {
+    getRepos(username);
+  }, [getRepos, username]);
+
+  return (
+    <div className="profile-github">
+      <h2 className="text-primary my-1">Github Repos</h2>
+      {repos === null ? (
+        <Spinner />
+      ) : (
+        repos.map((repo) => (
+          <div key={repo._id} className="repo bg-white p-1 my-1">
+            <h4>
+              <a href={repo.html_url} target="_blank" rel="noopener noreferrer">
+                {repo.name}
+              </a>
+            </h4>
+            <p>{repo.description}</p>
+            <div>
+              <ul>
+                <li className="badge badge-primary">
+                  Stars: {repo.stargazers_count}
+                </li>
+                <li className="badge badge-dark">
+                  Stars: {repo.watchers_count}
+                </li>
+                <li className="badge badge-white">Stars: {repo.forks_count}</li>
+              </ul>
+            </div>
+          </div>
+        ))
+      )}
+    </div>
+  );
+};
+
+ProfileGithub.propTypes = {
+  getRepos: PropTypes.func.isRequired,
+  repos: PropTypes.object.isRequired,
+  username: PropTypes.string.isRequired
+};
+
+const mapStateToProps = (state) => ({
+  repos: state.repos
+});
+
+export default connect(mapStateToProps, { getRepos })(ProfileGithub);
