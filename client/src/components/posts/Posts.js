74ccf11ed44a403ce1469d990ba50feac69dc27a
Wei Sheng Tan <tanweisheng02@gmail.com>
2020-08-21 01:46:14 -0700

    post item

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -4,13 +4,29 @@ import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
 import { Link } from "react-router-dom";
 import { getPosts } from "../../actions/post";
+import PostItem from "./PostItem";
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
   useEffect(() => {
     getPosts();
   }, [getPosts]);
 
-  return <div></div>;
+  return loading ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 className="large text-primary">Posts</h1>
+      <p className="lead">
+        <i className="fas fa-user" /> Welcome to this community
+      </p>
+      {/*PostForm*/}
+      <div className="posts">
+        {posts.map((post) => (
+          <PostItem key={post._id} post={post} />
+        ))}
+      </div>
+    </Fragment>
+  );
 };
 
 Posts.propTypes = {
