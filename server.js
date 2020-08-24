1244b9cee2b3bfff9902837437f34893f560c297
Wei Sheng Tan <tanweisheng02@gmail.com>
2020-08-24 10:03:33 -0700

    deploy ready

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
 // Connect DB
 connectDB();
-
-app.get("/", (req, res) => res.send("API is running..."));
 
 // Init Middleware
 app.use(express.json({ extended: false }));
@@ -16,6 +15,16 @@ app.use("/api/users", require("./routes/api/users"));
 app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/auth", require("./routes/api/auth"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 // PORT default to 5000
 const PORT = process.env.PORT || 5000;
